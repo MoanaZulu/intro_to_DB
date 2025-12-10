2e0e2a42f151ff0051a3680f08cbe166db1ce321
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-12-10 12:51:15 +0200

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -6,3 +6,9 @@ USE alx_book_store;
 SELECT TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES
 WHERE TABLE_SCHEMA = 'alx_book_store';
+
+-- Use the database
+USE alx_book_store;
+
+-- List all tables in the database
+SHOW TABLES;
