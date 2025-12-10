6275078fd02eb02d8267a49d65ee94ebc77ae3f3
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-12-10 12:55:17 +0200

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -4,3 +4,10 @@ USE alx_book_store;
 -- Insert a single customer row
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+-- Use the database
+USE alx_book_store;
+
+-- Insert a single customer row
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
