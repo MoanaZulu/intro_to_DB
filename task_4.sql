aefba59e76cbc68abcc4e7306defc0b6133f5933
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-12-10 12:52:54 +0200

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,8 @@
+-- Use the database
+USE alx_book_store;
+
+-- Print full description of the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
